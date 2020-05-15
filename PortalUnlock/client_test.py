b5dfbccc7beedfd2bbdcc14be690ad183c23487b
Alexandre <alexandrelebegue12@gmail.com>
2020-05-15 17:58:04 +0200

    Video can run multiple times

--- PortalUnlock/client_test.py
+++ PortalUnlock/client_test.py
@@ -3,49 +3,97 @@
 
 import socket, sys, os
 import sys
-from  video_client import video_client_thread
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
 
-HOST = '127.0.0.1'
-PORT = 8585
-
-# 1) création du socket :
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# 2) envoi d'une requête de connexion au serveur :
-try:
-  mySocket.connect((HOST, PORT))
-except socket.error:
-  print("La connexion a échoué.")
-  sys.exit()
-print("Connexion établie avec le serveur.")
-
-# 3) Dialogue avec le serveur :
-v = video_client_thread()
-video_stream = False
+from threading import Thread
 
 
-while 1:
-  print ("Waiting msg ...")
-  msgServeur = mySocket.recv(1024).decode("Utf8")
-  print("S>", msgServeur)
-  if msgServeur.upper() == "VIDEO":
-      msgClient = "VIDEORESP"
-      mySocket.send(msgClient.encode("Utf8"))
+def VideoStream(duration) :
+    print ("Starting Thread ...")
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 8485))
+    connection = client_socket.makefile('wb')
 
-      v.start()
-  elif msgServeur.upper() == "VIDEOSTOP":
-      msgClient = "VIDEORESPSTOP"
-      mySocket.send(msgClient.encode("Utf8"))
-      v.exit()
-  elif msgServeur.upper() == "QUIT":
-      msgClient = "QUITRESP"
-      mySocket.send(msgClient.encode("Utf8"))
-      mySocket.close()
-      # 4) Fermeture de la connexion :
-      print("Connexion interrompue.")
-      break
-  else :
-      msgClient = input("C> ")
-      mySocket.send(msgClient.encode("Utf8"))
+    cam = cv2.VideoCapture(0)
 
-print("END.")
+    cam.set(3, 320);
+    cam.set(4, 240);
+
+    img_counter = 0
+
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    start = time.time()
+    timer = 0
+    while timer < duration:
+        ret, frame = cam.read()
+        result, frame = cv2.imencode('.jpg', frame, encode_param)
+    #    data = zlib.compress(pickle.dumps(frame, 0))
+        data = pickle.dumps(frame, 0)
+        size = len(data)
+
+
+        #print("{}: {}".format(img_counter, size))
+        client_socket.sendall(struct.pack(">L", size) + data)
+        img_counter += 1
+        timer = time.time() - start
+
+
+    cam.release()
+    client_socket.close()
+    print("Quitting thread")
+
+
+def main() :
+    HOST = '127.0.0.1'
+    PORT = 8585
+
+    # 1) création du socket :
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 2) envoi d'une requête de connexion au serveur :
+    try:
+      mySocket.connect((HOST, PORT))
+    except socket.error:
+      print("La connexion a échoué.")
+      sys.exit()
+    print("Connexion établie avec le serveur.")
+
+    # 3) Dialogue avec le serveur :
+    video_stream = False
+
+
+    while 1:
+      print ("Waiting msg ...")
+      msgServeur = mySocket.recv(1024).decode("Utf8")
+      print("S>", msgServeur)
+      if msgServeur.upper() == "VIDEO":
+          msgClient = "VIDEORESP"
+          mySocket.send(msgClient.encode("Utf8"))
+          v = Thread(target  = VideoStream, args =(10, ))
+          v.start()
+      elif msgServeur.upper() == "VIDEOSTOP":
+          msgClient = "VIDEORESPSTOP"
+          mySocket.send(msgClient.encode("Utf8"))
+          v.exit()
+      elif msgServeur.upper() == "QUIT":
+          msgClient = "QUITRESP"
+          mySocket.send(msgClient.encode("Utf8"))
+          mySocket.close()
+          # 4) Fermeture de la connexion :
+          print("Connexion interrompue.")
+          break
+      else :
+          msgClient = input("C> ")
+          mySocket.send(msgClient.encode("Utf8"))
+
+
+
+print("Launching ...")
+main()
+print("Done.")
--- PortalUnlock/server_test.py
+++ PortalUnlock/server_test.py
@@ -1,51 +1,116 @@
 # Définition d'un serveur réseau rudimentaire
 # Ce serveur attend la connexion d'un client
+# cd Documents/Developpement/Git/RaspPortail/PortalUnlock
+# python -m server_test.py
 
 import socket, sys
-from  video_server import video_server_thread
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
 
-HOST = '127.0.0.1'
-PORT = 8585
-counter =0	 # compteur de connexions actives
+from threading import Thread
+import time
 
-# 1) création du socket :
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def VideoStream(duration):
+    print ("Starting Thread ...")
+    HOST=''
+    PORT=8485
 
-# 2) liaison du socket à une adresse précise :
-try:
-  mySocket.bind((HOST, PORT))
-except socket.error:
-  print("La liaison du socket à l'adresse choisie a échoué.")
-  sys.exit
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print('Socket created')
 
-while 1:
-  # 3) Attente de la requête de connexion d'un client :
-  print("Serveur prêt, en attente de requêtes ...")
-  mySocket.listen(5)
+    s.bind((HOST,PORT))
+    print('Socket bind complete')
+    s.listen(10)
+    print('Socket now listening')
 
-  # 4) Etablissement de la connexion :
-  connexion, adresse = mySocket.accept()
-  counter +=1
-  print("Client connecté, adresse IP %s, port %s" % (adresse[0], adresse[1]))
+    conn,addr=s.accept()
 
-  # 5) Dialogue avec le client :
-  msgServeur ="VIDEO"
-  connexion.send(msgServeur.encode("Utf8"))
-  v = video_server_thread()
-  while 1:
-    print ("Waiting msg ...")
-    msgClient = connexion.recv(1024).decode("Utf8")
-    print("C>", msgClient)
-    if msgClient.upper() == "VIDEORESP":
-        v.start()
-    elif msgClient.upper() == "VIDEORESPSTOP":
-        v.exit()
-    elif msgClient.upper() == "QUITRESP":
-        print("Connexion interrompue.")
-        connexion.close()
-        break
+    data = b""
+    payload_size = struct.calcsize(">L")
+    #print("payload_size: {}".format(payload_size))
+    start = time.time()
+    timer = 0
+    while timer < duration:
+        while len(data) < payload_size:
+            #print("Recv: {}".format(len(data)))
+            data += conn.recv(4096)
 
-    msgServeur = input("S> ")
+        #print("Done Recv: {}".format(len(data)))
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        #print("msg_size: {}".format(msg_size))
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        cv2.imshow('ImageWindow',frame)
+        cv2.waitKey(1)
+        timer = time.time() - start
+
+    cv2.destroyAllWindows()
+    s.close()
+    print("Quitting thread")
+
+
+def main():
+    HOST = '127.0.0.1'
+    PORT = 8585
+    counter =0	 # compteur de connexions actives
+
+    # 1) création du socket :
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 2) liaison du socket à une adresse précise :
+    try:
+      mySocket.bind((HOST, PORT))
+    except socket.error:
+      print("La liaison du socket à l'adresse choisie a échoué.")
+      sys.exit
+
+
+    # 3) Attente de la requête de connexion d'un client :
+    print("Serveur prêt, en attente de requêtes ...")
+    mySocket.listen(5)
+
+    # 4) Etablissement de la connexion :
+    connexion, adresse = mySocket.accept()
+    counter +=1
+    print("Client connecté, adresse IP %s, port %s" % (adresse[0], adresse[1]))
+
+    # 5) Dialogue avec le client :
+    msgServeur ="VIDEO"
     connexion.send(msgServeur.encode("Utf8"))
 
-Print("END.")
+
+    while 1:
+        print ("Waiting msg ...")
+        msgClient = connexion.recv(1024).decode("Utf8")
+        print("C>", msgClient)
+        if msgClient.upper() == "VIDEORESP":
+            v = Thread(target  = VideoStream, args =(10, ))
+            v.start()
+        elif msgClient.upper() == "VIDEORESPSTOP":
+            v.exit()
+        elif msgClient.upper() == "QUITRESP":
+            print("Connexion interrompue.")
+            connexion.close()
+            break
+
+        msgServeur = input("S> ")
+        connexion.send(msgServeur.encode("Utf8"))
+
+
+
+
+print("Launching ...")
+main()
+print("Done.")
